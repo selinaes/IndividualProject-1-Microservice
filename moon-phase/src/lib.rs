6dae75613e40a4b838926f665ca1eeaf23829fdd
Jiawei Liu (Selina) <selinaslytherin@126.com>
2023-02-08 03:12:20 +0000

    try to add api request to moon phase

--- moon-phase/src/lib.rs
+++ moon-phase/src/lib.rs
@@ -1,14 +1,38 @@
 /* A library that returns back moon phase given day and location */
-use reqwest;
 
-pub fn get_moon_phase(day: u8, month: u8, year: u16, lat: f32, lon: f32) -> String {
-    let url = format!(
-        "https://api.usno.navy.mil/rstt/oneday?date={}/{}/{}&coords={},{}",
-        month, day, year, lat, lon
-    );
-    let body = reqwest::blocking::get(&url).unwrap().text().unwrap();
-    let phase = body.split("Phase: ").collect::<Vec<&str>>()[1]
-        .split("")
-        .collect::<Vec<&str>>()[0];
-    phase.to_string()
+use chrono::{DateTime, Utc};
+use reqwest
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct MoonPhasesData {
+    apiversion: String,
+    day: i32,
+    month: i32,
+    numphases: i32,
+    phasedata: Vec<MoonPhase>,
+    year: i32,
+}
+
+#[derive(Deserialize, Debug)]
+struct MoonPhase {
+    day: i32,
+    month: i32,
+    phase: String,
+    time: String,
+    year: i32,
+}
+
+pub async fn get_moon_phase(date: DateTime<Utc>) -> Result<String, reqwest::Error> {
+
+    let moon_phases_data: MoonPhasesData = reqwest::get(&format!(
+        "https://aa.usno.navy.mil/api/moon/phases/date?date={}&nump=48",
+        date.format("%Y-%m-%d")
+    ))
+        .await?
+        .json::<MoonPhasesData>()
+        .await?;
+
+
+    Ok(moon_phases_data.phasedata[0].phase)
 }
--- moon-phase/src/main.rs
+++ moon-phase/src/main.rs
@@ -1,3 +1,7 @@
-/* An actix microservice that has two routes 
-A. 
+/* An actix microservice that has three routes
+A. / that returns a hello page with moon image
+B. /moon that returns the current moon phase
+C. /moon?date=YYYY-MM-DD that returns the moon phase for the given date
 */
+
+use actix_web::{get, middleware, web, App, HttpResponse, HttpServer, Responder};
